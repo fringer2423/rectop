2c079a97d47682de4c6c60c5bb8e7bc8684b4792
Alexandra <sashatru99@gmail.com>
2022-10-13 11:27:09 +0300

    fix front

---
+++ frontend/src/components/Circles.js
@@ -0,0 +1,123 @@
+import React from 'react';
+
+const Circles = () =>
+   <div className="d-flex flex-row flex-wrap">
+   <div
+       className="card border-0 col-lg-3 align-items-center">
+       <img alt='' className="w-50"
+            src="/images/main/mainCard41Img.png"/>
+       <div className="card-body">
+           <h4 className="card-title text-center m-2">
+               Яндекс Карты
+           </h4>
+           <p className="card-text text-center">
+               <small className="text-muted">
+                   55 млн чел./месяц
+               </small>
+           </p>
+       </div>
+       </div>
+       <div
+           className="card border-0 col-lg-3 align-items-center">
+           <img alt='' className="w-50"
+                src="/images/main/mainCard42Img.png"/>
+           <div className="card-body">
+               <h4 className="card-title text-center m-2">
+                   Google Карты
+               </h4>
+               <p className="card-text text-center">
+                   <small className="text-muted">
+                       30 млн чел./месяц
+                   </small>
+               </p>
+           </div>
+       </div>
+       <div
+           className="card border-0 col-lg-3 align-items-center">
+           <img alt='' className="w-50" src="/images/main/mainCard43Img.png"/>
+           <div className="card-body">
+               <h4 className="card-title text-center m-2">
+                   2ГИС
+               </h4>
+               <p className="card-text text-center">
+                   <small className="text-muted">
+                       30 млн чел./месяц
+                   </small>
+               </p>
+           </div>
+       </div>
+       <div
+           className="card border-0 col-lg-3 align-items-center">
+           <img alt='' className="w-50" src="/images/main/mainCard44Img.png"/>
+           <div className="card-body">
+               <h4 className="card-title text-center m-2">
+                   Wildberries
+               </h4>
+               <p className="card-text text-center">
+                   <small className="text-muted">
+                       92 млн чел./месяц
+                   </small>
+               </p>
+           </div>
+       </div>
+       <div
+           className="card border-0 col-lg-3 lign-items-center">
+           <img alt='' className="w-50" src="/images/main/mainCard45Img.png"/>
+           <div className="card-body">
+               <h4 className="card-title text-center m-2">
+                   Zoon
+               </h4>
+               <p className="card-text text-center">
+                   <small className="text-muted">
+                       24 млн чел./месяц
+                   </small>
+               </p>
+           </div>
+       </div>
+       <div
+           className="card border-0 col-lg-3 align-items-center">
+           <img alt='' className="w-50" src="/images/main/mainCard46Img.png"/>
+           <div className="card-body">
+               <h4 className="card-title text-center m-2">
+                   OZON
+               </h4>
+               <p className="card-text text-center">
+                   <small className="text-muted">
+                       80 млн чел./месяц
+                   </small>
+               </p>
+           </div>
+       </div>
+         <div
+          className="card border-0 col-lg-3 align-items-center">
+                   <img alt='' className="w-50" src="/images/main/mainCard47Img.png"/>
+                   <div className="card-body">
+                       <h4 className="card-title text-center m-2">
+                           Yell
+                       </h4>
+                       <p className="card-text text-center">
+                           <small className="text-muted">
+                               3 млн чел./месяц
+                           </small>
+                       </p>
+                   </div>
+               </div>
+               <div
+                   className="card border-0 col-lg-3 align-items-center">
+                   <img alt='' className="w-50"
+                        src="/images/main/mainCard48Img.png"/>
+                   <div className="card-body">
+                       <h4 className="card-title text-center m-2">
+                           Flamp
+                       </h4>
+                       <p className="card-text text-center">
+                           <small className="text-muted">
+                               50 млн чел./месяц
+                           </small>
+                       </p>
+                   </div>
+               </div>
+            </div>
+
+
+export default Circles
