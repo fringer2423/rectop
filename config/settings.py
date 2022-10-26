e7dc60d5258dbd605882b92f1dd7fd4c03846594
Vladislav Aganin <vlad_aganin33@hotmail.com>
2022-10-26 13:48:29 +0300

    docker config

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,6 @@
-from local_settings import *
+import os
+
+
 
 from datetime import timedelta
 from pathlib import Path
@@ -12,6 +14,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ==============================================================================
 # APPS SETTINGS
 # ==============================================================================
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = int(os.environ.get("DEBUG", default=0))
+
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -120,11 +128,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # ==============================================================================
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
