f0d5732d636c168370a51b361322a00a5404fcc0
AlexTrubkina <sashatru99@gmail.com>
2022-11-08 15:04:22 +0300

    изменены компоненты carousel и purplecircle

--- frontend/src/components/Brands.js
+++ frontend/src/components/Brands.js
@@ -68,20 +68,93 @@ const Brands = () => {
             </div>
             <div id="slider-for-icons">
                 <Carousel>
-                {brands.map(brand => {
-                    return(
-                        <div className="brand-icon" key={brand.title}>
+                    <div className="slide" >
+                        <div className="brand-icon">
                             <div className="icon-in-circle">
                                 <img
-                                    src={brand.src}
-                                    alt={brand.title}
+                                    src="/images/main/image49.png"
+                                    alt="Yandex"
                                 />
                             </div>
-                            <h5>{brand.title}</h5>
-                            <p>{brand.text}</p>
+                            <h5>Яндекс.Карты</h5>
+                            <p>55 млн чел./месяц</p>
                         </div>
-                    )
-                })}
+                        <div className="brand-icon">
+                            <div className="icon-in-circle">
+                                <img
+                                    src="/images/main/image50.png"
+                                    alt="Google"
+                                />
+                            </div>
+                            <h5>Google карты</h5>
+                            <p>30 млн чел./месяц</p>
+                        </div>
+                    </div>
+                    <div className="slide">
+                        <div className="brand-icon">
+                            <div className="icon-in-circle">
+                                <img
+                                    src="/images/main/image51.png"
+                                    alt="2ГИС"
+                                />
+                            </div>
+                            <h5>2ГИС</h5>
+                            <p>30 млн чел./месяц</p>
+                        </div>
+                        <div className="brand-icon">
+                            <div className="icon-in-circle">
+                                <img
+                                    src="/images/main/image52.png"
+                                    alt="Wildberries"
+                                />
+                            </div>
+                            <h5>Wildberries</h5>
+                            <p>92 млн чел./месяц</p>
+                        </div>
+                    </div>
+                    <div className="slide">
+                        <div className="brand-icon">
+                            <div className="icon-in-circle">
+                                <img
+                                    src="/images/main/image54.png"
+                                    alt="Zoon"
+                                />
+                            </div>
+                            <h5>Zoon</h5>
+                            <p>24 млн чел./месяц</p>
+                        </div>
+                        <div className="brand-icon">
+                            <div className="icon-in-circle">
+                                <img
+                                    src="/images/main/image55.png"
+                                    alt="OZON"
+                                />
+                            </div>
+                            <h5>OZON</h5>
+                            <p>80 млн чел./месяц</p>
+                        </div>
+                    </div>
+                    <div className="slide">
+                        <div className="brand-icon">
+                            <div className="icon-in-circle">
+                                <img
+                                    src="/images/main/image56.png"
+                                    alt="Yell"
+                                />
+                            </div>
+                            <h5>Yell</h5>
+                            <p>3 млн чел./месяц</p>
+                        </div>
+                        <div className="brand-icon">
+                            <div className="icon-in-circle">
+                                <img src="/images/main/image57.png"
+                                        alt="Flamp"
+                                />
+                            </div>
+                            <h5>Flamp</h5>
+                            <p>50 млн чел./месяц</p>
+                        </div>
+                    </div>
                 </Carousel>
             </div>
         </>
--- frontend/src/components/Carousel.js
+++ frontend/src/components/Carousel.js
@@ -1,28 +1,37 @@
 import React, { useEffect, useState, Children, cloneElement } from 'react';
 import '../css/carousel.css';
 
+import {faArrowRightLong} from '@fortawesome/free-solid-svg-icons';
+import {faArrowLeftLong} from '@fortawesome/free-solid-svg-icons';
 
-const PAGE_WIDTH = 233
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+
+const PAGE_WIDTH = 300
 
 const Carousel = ({ children }) => {
-  const [pages, setPages] = useState([])
-  const [offset, setOffset] = useState(0)
+  const [pages, setPages] = useState([]);
+  const [offset, setOffset] = useState(0);
+
 
   const handleLeftArrowClick = () => {
     setOffset((currentOffset) => {
-      const newOffset = currentOffset + PAGE_WIDTH
-      console.log(newOffset)
-      return Math.min(newOffset, 0)
+      const newOffset = currentOffset + PAGE_WIDTH;
+      console.log(newOffset);
+
+      return Math.min(newOffset, 500);
     })
   }
   const handleRightArrowClick = () => {
     setOffset((currentOffset) => {
-      const newOffset = currentOffset - PAGE_WIDTH
+      const newOffset = currentOffset - PAGE_WIDTH;
 
-      const maxOffset = -(PAGE_WIDTH * (pages.length - 1))
+      const maxOffset = -(500 + (children.length));
 
-      console.log(newOffset, maxOffset)
-      return Math.max(newOffset, maxOffset)
+
+
+      console.log(newOffset, maxOffset);
+      return Math.max(newOffset, maxOffset);
     })
   }
 
@@ -31,17 +40,24 @@ const Carousel = ({ children }) => {
       Children.map(children, (child) => {
         return cloneElement(child, {
           style: {
-            minWidth: `${child.scrollWidth}px`,
-            maxWidth: `${child.scrollWidth}px`,
-            height: `${child.scrollHeight}px`,
+            minWidth: `300px`,
+            maxWidth: `300px`,
+            height: `${child.scrollHeight}`,
           },
         })
       })
     )
-  }, [children])
+  }, [])
+
 
   return (
-    <div className="carousel-container">
+    <div
+        className="carousel-container"
+        style={{
+              height: '100%',
+              width: '300px',
+        }}
+    >
       <div className="slider-carousel">
         <div
           className="slide"
@@ -52,8 +68,30 @@ const Carousel = ({ children }) => {
           {pages}
         </div>
       </div>
-      <div className="arrow" onClick={handleLeftArrowClick}>-</div>
-      <div className="arrow" onClick={handleRightArrowClick}>- </div>
+      <div className="arrows-in-carousel">
+        <div>
+          <FontAwesomeIcon
+              icon={faArrowLeftLong} className="arrow-carousel" onClick={handleLeftArrowClick }/>
+        </div>
+          <div className="number-of-slides">
+            {
+                pages.map(page => {
+                    return (
+                        <div
+                            className={
+                                 'circle-of-page'
+                            }
+                            key={page.index}>
+                        </div>
+                    )
+                })
+            }
+          </div>
+          <div>
+            <FontAwesomeIcon
+                icon={faArrowRightLong} className="arrow-carousel" onClick={handleRightArrowClick}/>
+          </div>
+      </div>
     </div>
   )
 }
