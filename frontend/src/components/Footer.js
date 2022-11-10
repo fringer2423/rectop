3d3d04edcc2eaa1a9586c4ca28dad053aa1bbbe7
AlexTrubkina <sashatru99@gmail.com>
2022-11-10 12:21:23 +0300

    added footer.css changed Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -12,12 +12,14 @@ import {
     faVk
 } from '@fortawesome/free-brands-svg-icons';
 
+import "../css/footer.css"
+
 function Footer() {
     return (
-        <Navbar bg="primary" variant="dark">
-            <Container>
-                <Nav className="d-flex flex-column text-white w-25 me-5 gap-5 mb-5">
-                    <Nav className="d-flex flex-column gap-3">
+        <Navbar variant="dark" className="navbar-footer">
+            <Container className="container-footer">
+                <div className="footer-first-nav">
+                    <div className="footer-navbar-logo">
                         <Navbar.Brand>
                             <img
                                 src="/images/logofooter.png"
@@ -27,26 +29,58 @@ function Footer() {
                         <Nav.Item>
                             Мы используем куки, это позволяет улучшить ваш пользовательский опыт
                         </Nav.Item>
-                    </Nav>
-                    <Nav className="d-flex flex-column gap-3">
+                    </div>
+                    <div className="footer-contacts-navs-mobile">
+                        <div className="nav-footer-mobile">
+                            <Nav.Item><b>Навигация</b></Nav.Item>
+                            <Nav.Link href="/">Главная</Nav.Link>
+                            <Nav.Link>О нас</Nav.Link>
+                            <Nav.Link>Как мы работаем</Nav.Link>
+                            <Nav.Link>Тарифы</Nav.Link>
+                            <Nav.Link>Обратная связь</Nav.Link>
+                            <Nav.Link>FAQ</Nav.Link>
+                        </div>
+                        <div className="contacts-footer-mobile">
+                            <Nav.Item><b>Контакты</b></Nav.Item>
+                            <Nav.Link>business@rectop.ru</Nav.Link>
+                            <Nav.Link>+ 7 (999) 888-77-66</Nav.Link>
+                            <Nav.Link>Политика конфиденциальности</Nav.Link>
+                            <Nav.Link>Лицензионный договор</Nav.Link>
+                            <div className="icons-footer">
+                                <FontAwesomeIcon icon={faTelegram}/>
+                                <FontAwesomeIcon icon={faVk}/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer-reference">
                         <Nav.Item>
                             © 2018–2022 Rectop - агрегатор отзывов
                         </Nav.Item>
                         <Nav.Item>
                             Дизайнер - Чернев Максим (@maxim_235)
                         </Nav.Item>
-                    </Nav>
-                </Nav>
-                <Nav className="me-auto d-flex flex-column mt-5  mb-5 text-white">
-                    <Nav.Item><b>Навигация</b></Nav.Item>
-                    <Nav.Link href="/">Главная</Nav.Link>
-                    <Nav.Link>О нас</Nav.Link>
-                    <Nav.Link>Как мы работаем</Nav.Link>
-                    <Nav.Link>Тарифы</Nav.Link>
-                    <Nav.Link>Обратная связь</Nav.Link>
-                    <Nav.Link>FAQ</Nav.Link>
-                </Nav>
-
+                    </div>
+                </div>
+                    <div className="nav-footer-computer">
+                        <Nav.Item><b>Навигация</b></Nav.Item>
+                        <Nav.Link href="/">Главная</Nav.Link>
+                        <Nav.Link>О нас</Nav.Link>
+                        <Nav.Link>Как мы работаем</Nav.Link>
+                        <Nav.Link>Тарифы</Nav.Link>
+                        <Nav.Link>Обратная связь</Nav.Link>
+                        <Nav.Link>FAQ</Nav.Link>
+                    </div>
+                    <div className="contacts-footer-computer">
+                        <Nav.Item><b>Контакты</b></Nav.Item>
+                        <Nav.Link>business@rectop.ru</Nav.Link>
+                        <Nav.Link>+ 7 (999) 888-77-66</Nav.Link>
+                        <Nav.Link>Политика конфиденциальности</Nav.Link>
+                        <Nav.Link>Лицензионный договор</Nav.Link>
+                        <div className="icons-footer">
+                            <FontAwesomeIcon icon={faTelegram}/>
+                            <FontAwesomeIcon icon={faVk}/>
+                        </div>
+                    </div>
             </Container>
         </Navbar>
     )
