e29f2964972c91a18ba34706f805c66f5bef1d7e
AlexTrubkina <sashatru99@gmail.com>
2022-11-11 13:36:16 +0300

    deleted carousel.js and added react-bootstrap carousel

--- frontend/src/components/Prices.js
+++ frontend/src/components/Prices.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "../css/prices.css"
 
-import Carousel from './Carousel.js'
+import {Carousel} from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
-const Prices = (props) => {
+import {faArrowRightLong} from '@fortawesome/free-solid-svg-icons';
+import {faArrowLeftLong} from '@fortawesome/free-solid-svg-icons';
+
+const Prices = () => {
     return (
         <>
 
@@ -89,91 +93,101 @@ const Prices = (props) => {
             </div>
 
 
-            <div id="slider-for-prices">
-                <Carousel>
 
-                    <div className="table-for-price-first">
-                        <h3><b>“Генератор отзывов”</b></h3>
-                        <h5>1 филиал</h5>
-                        <h4><b>1200 РУБ./МЕС.</b></h4>
-                        <div className="block-in-table">
-                            <b>
-                                Производите сбор отзывов </b> от ваших клиентов при помощи QR-кодов на точках продаж
-                        </div>
-                        <div className="text-in-price">
-                            Сбор отзывов
-                            <hr/>
-                            Оповещения в Телеграме
-                            <hr/>
-                            Отчетность в Телеграме
-                            <hr/>
-                            Система кэшбека
-                            <hr/>
-                            Статистика
-                            <hr/>
-                            Обратная связь
-                            <hr/>
-                            Поддержка 24/7
-                            <hr/>
-                            Формирование qr-кода
-                            <hr/>
-                        </div>
-                        <button className="black-button"><b>Приобрести тариф</b></button>
-                    </div>
+            <div id="slider-for-prices" >
+                <Carousel className="carousel-for-prices" interval={null} >
+                    <Carousel.Item >
 
-                    <div className="table-for-price-second">
-                        <h3><b>Какое-то название</b></h3>
-                        <h5>1 филиал</h5>
-                        <h4><b>1200 РУБ./МЕС.</b></h4>
-                        <div className="block-in-table">
-                            <b>Работа с отзывами</b> на крупнейших интернет-площадках и сервисах
+                        <div className="table-for-price-first">
+                            <h3><b>“Генератор отзывов”</b></h3>
+                            <h5>1 филиал</h5>
+                            <h4><b>1200 РУБ./МЕС.</b></h4>
+                            <div className="block-in-table">
+                                <b>
+                                    Производите сбор отзывов </b> от ваших клиентов при помощи QR-кодов на точках продаж
+                            </div>
+                            <div className="text-in-price">
+                                Сбор отзывов
+                                <hr/>
+                                Оповещения в Телеграме
+                                <hr/>
+                                Отчетность в Телеграме
+                                <hr/>
+                                Система кэшбека
+                                <hr/>
+                                Статистика
+                                <hr/>
+                                Обратная связь
+                                <hr/>
+                                Поддержка 24/7
+                                <hr/>
+                                Формирование qr-кода
+                                <hr/>
+                            </div>
+                            <button className="black-button"><b>Приобрести тариф</b></button>
                         </div>
-                        <div className="text-in-price">
-                            Аналитика по филиалу
-                            <hr/>
-                            Постоянная актуализация
-                            <hr/>
-                            Система кэшбека
-                            <hr/>
-                            Скидки от партнеров
-                            <hr/>
-                            Поддержка 24/7
-                            <hr/>
-                            Ответы на отзывы в одном месте
-                            <hr/>
-                            Обратная связь
-                            <hr/>
-                            Подробная статистика
-                            <hr/>
-                        </div>
-                        <button className="black-button"><b>Приобрести тариф</b></button>
-                    </div>
 
-                    <div className="table-for-price-third">
-                        <h3><b>Репутация под ключ</b></h3>
-                        <h5>1 филиал</h5>
-                        <h4><b>ОТ 4000 РУБ./МЕС.</b></h4>
-                        <div className="block-in-table">
-                            <b>Работа с отзывами</b> на крупнейших интернет-площадках и сервисах
+                    </Carousel.Item>
+                    <Carousel.Item >
+
+                        <div className="table-for-price-second">
+                            <h3><b>Какое-то название</b></h3>
+                            <h5>1 филиал</h5>
+                            <h4><b>1200 РУБ./МЕС.</b></h4>
+                            <div className="block-in-table">
+                                <b>Работа с отзывами</b> на крупнейших интернет-площадках и сервисах
+                            </div>
+                            <div className="text-in-price">
+                                Аналитика по филиалу
+                                <hr/>
+                                Постоянная актуализация
+                                <hr/>
+                                Система кэшбека
+                                <hr/>
+                                Скидки от партнеров
+                                <hr/>
+                                Поддержка 24/7
+                                <hr/>
+                                Ответы на отзывы в одном месте
+                                <hr/>
+                                Обратная связь
+                                <hr/>
+                                Подробная статистика
+                                <hr/>
+                            </div>
+                            <button className="black-button"><b>Приобрести тариф</b></button>
                         </div>
-                        <div className="text-in-price">
-                            Генератор и агрегатор отзывов
-                            <hr/>
-                            Ответы на отзывы нашими специалистами
-                            <hr/>
-                            Проверка выставленных отзывов
-                            <hr/>
-                            “Спровоцируем” выставление отзывов
-                            <hr/>
-                            Подробная отчетность
-                            <hr/>
+
+                    </Carousel.Item>
+                    <Carousel.Item >
+
+                        <div className="table-for-price-third">
+                            <h3><b>Репутация под ключ</b></h3>
+                            <h5>1 филиал</h5>
+                            <h4><b>ОТ 4000 РУБ./МЕС.</b></h4>
+                            <div className="block-in-table">
+                                <b>Работа с отзывами</b> на крупнейших интернет-площадках и сервисах
+                            </div>
+                            <div className="text-in-price">
+                                Генератор и агрегатор отзывов
+                                <hr/>
+                                Ответы на отзывы нашими специалистами
+                                <hr/>
+                                Проверка выставленных отзывов
+                                <hr/>
+                                “Спровоцируем” выставление отзывов
+                                <hr/>
+                                Подробная отчетность
+                                <hr/>
                             Аналитика в реальном времени
-                        </div>
-                        <button className="black-button"><b>Приобрести тариф</b></button>
-                    </div>
+                            </div>
+                            <button className="black-button"><b>Приобрести тариф</b></button>
+                            </div>
 
-                </Carousel>
+                    </Carousel.Item>
+                    </Carousel>
             </div>
+            
         </>
     )
 }
