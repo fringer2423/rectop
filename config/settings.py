4f8a4d082e5374e009cad6c380ea69dab8386ab3
Vladislav Aganin <vlad_aganin33@hotmail.com>
2022-11-15 10:48:44 +0300

    Edited readme, changelog. Added email push config

--- config/settings.py
+++ config/settings.py
@@ -291,3 +291,14 @@ LOGGING = {
         },
     },
 }
+
+# ==============================================================================
+# EMAIL SETTINGS
+# ==============================================================================
+
+EMAIL_HOST = os.environ.get("EMAIL_HOST", local_settings.EMAIL_HOST)
+EMAIL_PORT = os.environ.get("EMAIL_PORT", local_settings.EMAIL_PORT)
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", local_settings.EMAIL_HOST_USER)
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", local_settings.EMAIL_HOST_PASSWORD)
+# EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", local_settings.EMAIL_USE_TLS)
+EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL", local_settings.EMAIL_USE_SSL)
