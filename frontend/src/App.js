f9617c68df2a39251ec4b754953a30327928efc9
AlexTrubkina <sashatru99@gmail.com>
2022-11-18 16:23:04 +0300

    added purity ui dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 
 import './index.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
+
+
 import HomeScreen from './screens/HomeScreen'
 import RegistrationPage from "./screens/RegistrationPage"
 import LogInPage from "./screens/LogInPage"
+
+import AuthLayout from "./layouts/Auth.js";
+import AdminLayout from "./layouts/Admin.js";
 
 import {Container} from 'react-bootstrap'
 
@@ -15,13 +18,9 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 function App() {
     return (
         <Router>
-            <Header/>
-            <Container>
-                <Route exact path='/' component={HomeScreen}/>
-                <Route path='/registration' component={RegistrationPage}/>
-                <Route path='/log-in' component={LogInPage}/>
-            </Container>
-            <Footer/>
+            <Route exact path='/' component={HomeScreen}/>
+            <Route path='/auth' component={AuthLayout}/>
+            <Route path='/admin' component={AdminLayout}/>
         </Router>
     );
 }
--- frontend/src/screens/LogInPage.js
+++ frontend/src/screens/LogInPage.js
@@ -1,21 +1,20 @@
 import React, {useState} from 'react';
 
 import {
-    Modal,
-    Button,
-    InputGroup,
-    Form
-} from 'react-bootstrap';
+  Box,
+  Flex,
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Switch,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-
-import {
-    faGoogle,
-    faVk,
-    faYandex
-} from '@fortawesome/free-brands-svg-icons';
-
-import "../css/registration.css"
+import signInImage from "../assets/img/signInImage.png";
 
 import {checkMail} from "../validators/validation.js"
 
@@ -23,6 +22,9 @@ import {LinkContainer} from 'react-router-bootstrap'
 
 
 const LogIn = () => {
+
+    const titleColor = useColorModeValue("teal.300", "teal.200");
+    const textColor = useColorModeValue("gray.400", "white");
 
     const [mail, setMail] = useState('');
     const [mailError, setMailError] = useState(false);
@@ -36,47 +38,123 @@ const LogIn = () => {
     }
 
     return (
-        <div className="log-in-page">
+        <Flex position='relative' mb='40px'>
+      <Flex
+        h={{ sm: "initial", md: "75vh", lg: "85vh" }}
+        w='100%'
+        maxW='1044px'
+        mx='auto'
+        justifyContent='space-between'
+        mb='30px'
+        pt={{ sm: "100px", md: "0px" }}>
+        <Flex
+          alignItems='center'
+          justifyContent='start'
+          style={{ userSelect: "none" }}
+          w={{ base: "100%", md: "50%", lg: "42%" }}>
+          <Flex
+            direction='column'
+            w='100%'
+            background='transparent'
+            p='48px'
+            mt={{ md: "150px", lg: "80px" }}>
+            <Heading color={titleColor} fontSize='32px' mb='10px'>
+              Welcome Back
+            </Heading>
+            <Text
+              mb='36px'
+              ms='4px'
+              color={textColor}
+              fontWeight='bold'
+              fontSize='14px'>
+              Enter your email and password to sign in
+            </Text>
+            <FormControl>
+              <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>
+                Email
+              </FormLabel>
+              <Input
+                borderRadius='15px'
+                mb='24px'
+                fontSize='sm'
+                type='text'
+                placeholder='Your email adress'
+                size='lg'
+              />
+              <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>
+                Password
+              </FormLabel>
+              <Input
+                borderRadius='15px'
+                mb='36px'
+                fontSize='sm'
+                type='password'
+                placeholder='Your password'
+                size='lg'
+              />
+              <FormControl display='flex' alignItems='center'>
+                <Switch id='remember-login' colorScheme='teal' me='10px' />
+                <FormLabel
+                  htmlFor='remember-login'
+                  mb='0'
+                  ms='1'
+                  fontWeight='normal'>
+                  Remember me
+                </FormLabel>
+              </FormControl>
+              <Button
+                fontSize='10px'
+                type='submit'
+                bg='teal.300'
+                w='100%'
+                h='45'
+                mb='20px'
+                color='white'
+                mt='20px'
+                _hover={{
+                  bg: "teal.200",
+                }}
+                _active={{
+                  bg: "teal.400",
+                }}>
+                SIGN IN
+              </Button>
+            </FormControl>
+            <Flex
+              flexDirection='column'
+              justifyContent='center'
+              alignItems='center'
+              maxW='100%'
+              mt='0px'>
+              <Text color={textColor} fontWeight='medium'>
+                Don't have an account?
+                <Link color={titleColor} as='span' ms='5px' fontWeight='bold'>
+                  Sign Up
+                </Link>
+              </Text>
+            </Flex>
+          </Flex>
+        </Flex>
+        <Box
+          display={{ base: "none", md: "block" }}
+          overflowX='hidden'
+          h='100%'
+          w='40vw'
+          position='absolute'
+          right='0px'>
+          <Box
+            bgImage={signInImage}
+            w='100%'
+            h='100%'
+            bgSize='cover'
+            bgPosition='50%'
+            position='absolute'
+            borderBottomLeftRadius='20px'></Box>
+        </Box>
+      </Flex>
+    </Flex>
 
-            <h1>Вход</h1>
-
-            <Form.Label className="mt-5"><b>Email</b></Form.Label>
-            <InputGroup className=" mb-3 w-75 ">
-                <Form.Control
-                    placeholder="Email"
-                    aria-label="Post"
-                    aria-describedby="basic-addon1"
-                    value={mail}
-                    onChange={(e) => setMail(e.target.value)}
-                />
-            </InputGroup>
-            <Form.Label><b>Пароль</b></Form.Label>
-            <InputGroup className=" mb-5 w-75 ">
-                <Form.Control
-                    placeholder="Пароль"
-                    aria-label="Password"
-                    aria-describedby="basic-addon1"
-                />
-            </InputGroup>
-            <div className="icons-of-registration-brands">
-                <FontAwesomeIcon icon={faGoogle} className="icon-google"/>
-                <FontAwesomeIcon icon={faVk} className="icon-vk"/>
-                <FontAwesomeIcon icon={faYandex} className="icon-yandex"/>
-            </div>
-
-            {mailError && <h1>Неверный почтовый адрес</h1>}
-
-            <LinkContainer to="/">
-                <button className="white-button">
-                    Выйти
-                </button>
-            </LinkContainer>
-            <button className="blue-button" onClick={handleErrorMail}>
-                Войти
-            </button>
-        </div>
-
-    )
+    );
 }
 
 export default LogIn;
