e8c396712d9756e6c8b826f7853e38eb9bd18b2e
AlexTrubkina <sashatru99@gmail.com>
2023-01-05 16:52:04 +0300

    qr code actions

--- frontend/src/actions/QRActions.js
+++ frontend/src/actions/QRActions.js
@@ -1,21 +1,22 @@
 import axios from "axios";
 
 import {
-    RATE_ADD_SUCCESS,
-    RATE_ADD_FAIL,
-    RATE_ADD_REQUEST,
-    RATE_CHANGE_FAIL,
-    RATE_CHANGE_REQUEST,
-    RATE_CHANGE_SUCCESS,
-    RATE_READ_FAIL,
-    RATE_READ_REQUEST,
-    RATE_READ_SUCCESS,
-} from "../constants/rateConstants";
+    QR_CREATE_REQUEST,
+    QR_CREATE_SUCCESS,
+    QR_CREATE_FAIL,
+    QR_ALL_REQUEST,
+    QR_ALL_FAIL,
+    QR_ALL_SUCCESS,
+    QR_GET_ONE_FAIL,
+    QR_GET_ONE_REQUEST,
+    QR_GET_ONE_SUCCESS,
+} from "../constants/QRCodeConstants";
 
-export const rateAdd = (rateType) => async (dispatch, getState) => {
+
+export const qrCodeCreate = (branchID) => async(dispatch, getState) => {
     try {
         dispatch({
-            type: RATE_ADD_REQUEST,
+            type: QR_CREATE_REQUEST,
         });
 
         const {
@@ -30,168 +31,160 @@ export const rateAdd = (rateType) => async (dispatch, getState) => {
         };
 
         const {data} = await axios.post(
-            "/api/rate/create",
-            {type: rateType},
+            `/api/qrcode/create/`,
+            {type: branchID},
             config,
-        );
-        
+        )
+
         dispatch({
-            type: RATE_ADD_SUCCESS, 
+            type: QR_CREATE_SUCCESS,
             payload: data,
         });
 
+
     } catch (error) {
+
         switch (error) {
             case 400:
                 dispatch({
-                    type: RATE_ADD_FAIL,
+                    type: QR_CREATE_FAIL,
                     payload: 'Ошибка при создании',
                 })
                 break;
             case 401:
                 dispatch({
-                    type: RATE_ADD_FAIL,
+                    type: QR_CREATE_FAIL,
                     payload: 'Пустой или неправильный токен',
                 })
                 break;
             case 403:
                 dispatch({
-                    type: RATE_ADD_FAIL,
+                    type: QR_CREATE_FAIL,
                     payload: 'Ошибка доступа',
                 })
                 break;
+
+            case 404:
+                dispatch({
+                    type: QR_CREATE_FAIL,
+                    payload: 'Филиал не найден',
+                })
+                break;
+
             case 406:
                 dispatch({
-                    type: RATE_ADD_FAIL,
-                    payload: 'У вас уже есть тариф',
+                    type: QR_CREATE_FAIL,
+                    payload: 'У вас уже есть код на данный филиал',
                 })
                 break;
             default:
                 dispatch({
-                    type: RATE_ADD_FAIL,
+                    type: QR_CREATE_FAIL,
                     payload: 'Произошла ошибка: ' + error,
                 })
                 break;
         }
+        
     }
 }
 
-export const rateRead = () => async (dispatch, getState) => {
+export const getAllCodes = () => async (dispatch) => {
     try {
         dispatch({
-            type: RATE_READ_REQUEST,
+            type: QR_ALL_REQUEST,
         });
-
-        const {
-            userLogin: {userInfo},
-        } = getState();
 
         const config = {
             headers: {
                 "Content-type": "application/json",
-                Authorization: `Bearer ${userInfo.token}`,
             },
         };
 
         const {data} = await axios.get(
-            'api/rate/read', 
+            `/api/qrcode/read/all`,
             config,
         );
 
         dispatch({
-            type: RATE_READ_SUCCESS,
+            type: QR_ALL_SUCCESS,
             payload: data,
-        })
+        });
 
     } catch (error) {
         switch (error) {
             case 400:
                 dispatch({
-                    type: RATE_READ_FAIL,
+                    type: QR_ALL_FAIL,
                     payload: 'Ошибка при запросе',
                 })
                 break;
-            case 403:
-                dispatch({
-                    type: RATE_READ_FAIL,
-                    payload: 'Ошибка доступа',
-                })
-                break;
+
             case 404:
                 dispatch({
-                    type: RATE_READ_FAIL,
-                    payload: 'Тариф не найден',
+                    type: QR_ALL_FAIL,
+                    payload: 'Коды не найдены',
                 })
                 break;
-        
+
             default:
                 dispatch({
-                    type: RATE_READ_FAIL,
+                    type: QR_ALL_FAIL,
                     payload: 'Произошла ошибка: ' + error,
                 })
                 break;
         }
-        
     }
-}
+} 
 
-export const rateUpdate = (rateType) =>  async (dispatch, getState) => {
+export const getOneCode = (codeID) => async (dispatch) => {
     try {
         dispatch({
-            type: RATE_CHANGE_REQUEST,
-        })
-
-        const {
-            userLogin: {userInfo},
-        } = getState();
+            type: QR_GET_ONE_REQUEST,
+        });
 
         const config = {
             headers: {
                 "Content-type": "application/json",
-                Authorization: `Bearer ${userInfo.token}`,
             },
         };
 
-        const {data} = await axios.put(
-            `api/rate/update`,
-            {type: rateType},
+        const {data} = await axios.get(
+            `/api/qrcode/read/${codeID}`,
             config,
         );
 
         dispatch({
-            type: RATE_CHANGE_SUCCESS,
+            type: QR_GET_ONE_SUCCESS,
             payload: data,
         });
 
-        dispatch({
-            type: RATE_READ_SUCCESS,
-            payload: data,
-        });
 
     } catch (error) {
         switch (error) {
             case 400:
                 dispatch({
-                    type: RATE_CHANGE_FAIL,
+                    type: QR_GET_ONE_FAIL,
                     payload: 'Ошибка при запросе',
                 })
                 break;
+
             case 403:
                 dispatch({
-                    type: RATE_CHANGE_FAIL,
+                    type: QR_GET_ONE_FAIL,
                     payload: 'Ошибка доступа',
                 })
                 break;
+
             case 404:
                 dispatch({
-                    type: RATE_CHANGE_FAIL,
-                    payload: 'Тариф не найден',
+                    type: QR_GET_ONE_FAIL,
+                    payload: 'Код не найден',
                 })
                 break;
-        
+
             default:
                 dispatch({
-                    type: RATE_CHANGE_FAIL,
+                    type: QR_GET_ONE_FAIL,
                     payload: 'Произошла ошибка: ' + error,
                 })
                 break;
